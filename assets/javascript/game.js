dc42db598cf340b3e90e83423062749176900105
rivendell <webwyler@gmail.com>
2019-07-05 00:05:47 -0500

    add JQuery CDN to head section, make sure clicking an image generates a console msg

---
+++ assets/javascript/game.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+    $('img').on('click', function() {
+
+        // make sure clicking an image works 
+        console.log("You clicked me!");
+
+
+    });
+
+    // $('.num').on('click', function() {
+    //     // console.log(event);
+    //     // check if number is clicked
+    //     // console.log(this.innerHTML);
+    //     // append numers into an empty string
+    
+    //     if (selectSecondNums === true) {
+    //         second_numbers = second_numbers + $(this).attr('val');
+    //         console.log(second_numbers, typeof second_numbers );
+    //     }
+    //     else {
+    //         first_numbers = first_numbers + $(this).attr('val');
+    //         console.log(first_numbers, typeof first_numbers);
+    //     }
+    // });
+
+});
+
+
