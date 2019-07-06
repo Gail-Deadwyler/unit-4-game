70a640f7ebec448e30d593b5b548ed2f0c10880b
rivendell <webwyler@gmail.com>
2019-07-05 23:32:43 -0500

    add code to keep track of wins, loses and add to HTML

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 
+    var total_score;
+    var num_to_beat;
+    var wins = 0;
+    var loses = 0;
+
     // $('img').on('click', function() {
 
     //     // make sure clicking an image works 
@@ -9,16 +14,47 @@ $(document).ready(function() {
     // generate a random number between 1 and 5 (inclusive) and show on HTML
     $('h3#numToBeat').text(Math.floor(Math.random() * 5) + 1);
 
+    num_to_beat = $('h3#numToBeat').text();
+    num_to_beat = parseInt(num_to_beat);
+    // alert(num_to_beat);
+
     // click on an image, generate a random number and show on HTML
     $('img.randnum').click(function() {
-
         $('h3#total_score').text(Math.floor(Math.random() * 5) + 1);
+
+        total_score = $('h3#total_score').text();
+        total_score = parseInt(total_score);
+
+
+        // keep track of wins and loses
+        console.log("what is num to beat ---", num_to_beat);
+        console.log("what is total score ---", total_score);
+
+        if (num_to_beat == total_score) {
+            wins++;            
+            $('#wins').text(wins);
+            // alert("you have" + wins + " wins");
+        }
+        else if (total_score > num_to_beat) {
+            loses++;
+            $('#loses').text(loses);
+            // alert("you have" + loses + " loses");
+        }
+        
+        // alert(total_score);
 
         // var CrystalNum = $(this).text(Math.floor(Math.random() * 5) + 1);
         // console.log(CrystalNum);
-
-
     });
+
+
+
+
+    
+
+    // keep track of image random score
+
+
 
 
     // var RandNum = $('<div>');
