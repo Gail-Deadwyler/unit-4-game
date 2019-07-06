8509ec9a7a92b96d8e5b6cd0cbf45868b85c8426
rivendell <webwyler@gmail.com>
2019-07-05 20:53:50 -0500

    click on the image and generate a random number, show on HTML, test case (1-5)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,24 @@
 $(document).ready(function() {
 
-    $('img').on('click', function() {
+    // $('img').on('click', function() {
 
-        // make sure clicking an image works 
-        console.log("You clicked me!");
-    });
+    //     // make sure clicking an image works 
+    //     console.log("You clicked me!");
+    // });
 
     // generate a random number between 1 and 5 (inclusive) and show on HTML
     $('h3#numToBeat').text(Math.floor(Math.random() * 5) + 1);
+
+    // click on an image, generate a random number and show on HTML
+    $('img.randnum').click(function() {
+
+        $('h3#total_score').text(Math.floor(Math.random() * 5) + 1);
+
+        // var CrystalNum = $(this).text(Math.floor(Math.random() * 5) + 1);
+        // console.log(CrystalNum);
+
+
+    });
 
 
     // var RandNum = $('<div>');
